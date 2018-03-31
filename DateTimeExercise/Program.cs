10e7055353d4a175ee826bbde9fc8df2d08f2ea5
Crystal <CwittleZ@gmail.com>
2018-03-31 12:49:44 -0700

    DateTimeExercise
    1. Prints the current date and time to the console.
    
    2. Asks the user for a number.
    
    3. Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.

---
+++ DateTimeExercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prints the current date and time to the console.
+            Console.WriteLine($"Right now it is: {DateTime.Now}");
+
+            // Asks the user for a number.
+            bool wholeNumber = true;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Please enter a number.");
+                    double userInput = Convert.ToDouble(Console.ReadLine());
+
+                    // Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+                    var addUserHours = DateTime.Now.AddHours(userInput);
+                    Console.WriteLine($"The date and time will be {addUserHours} after adding {userInput} hours to the current time.");
+                    wholeNumber = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Sorry, I could not understand your number, please try again and enter a whole number.");
+                    Console.WriteLine();
+                    wholeNumber = false;
+                }
+
+            } while (!wholeNumber);
+                
+            
+
+            Console.ReadKey();
+        }
+    }
+}
