8b289d2c2d81aaa45c65321fc645dc74b48cee4e
Crystal <CwittleZ@gmail.com>
2018-03-31 11:48:12 -0700

    InputOutputExercise
    1. Ask a user for a number.
    
    2. Log that number to a text file.
    
    3. Print the text file back to the user.

---
+++ InputOutputExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputOutputExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter any number!");
+            string userInput = Console.ReadLine();
+
+            // Logging userInput into IOExercise.txt file
+            File.WriteAllText(@"E:\Coding\Visual Studio\Projects\The Tech Academy\CSharpCourses\InputOutputExercise\IOExercise.txt", userInput);
+
+            // Reading userInput number from IOExercise.txt file
+            string answer = File.ReadAllText(@"E:\Coding\Visual Studio\Projects\The Tech Academy\CSharpCourses\InputOutputExercise\IOExercise.txt");
+
+            // Displaying answer to console
+            Console.WriteLine($"Your number was {answer}!");
+
+            Console.ReadKey();
+        }
+    }
+}
